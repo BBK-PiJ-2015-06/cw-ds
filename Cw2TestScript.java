784737dac54ac6524ad9d7cb508d8792c8be25dd
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-01 12:52:55 +0000

    TestScript created to test whether code written so far works. Simply added main and launch methods.

---
+++ Cw2TestScript.java
@@ -0,0 +1,9 @@
+public class Cw2TestScript {
+	public static void main(String[] args) {
+		Cw2TestScript launcher = new Cw2TestScript();
+		launcher.launch();
+	}
+	public void launch() {
+		ArrayList array1 = new ArrayList();
+	}
+}
