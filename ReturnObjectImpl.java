fec6a286f7ee63c33cd33d642341d49231ac9ecc
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-01 20:33:57 +0000

    New method printReturnObject() created to help with testing.

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -8,10 +8,6 @@ public class ReturnObjectImpl implements ReturnObject {
 		this.error = null;
 	}
 	
-	/**
-	 * Returns whether there has been an error
-	 * @return whether there has been an error
-	 */
 	public boolean hasError() {
 		boolean output = false;
 		if(this.error != null) {
@@ -20,14 +16,6 @@ public class ReturnObjectImpl implements ReturnObject {
 		return output;
 	}
 
-	/**
-	 * Returns the error message. 
-	 * 
-	 * This method must return NO_ERROR if and only if
-	 * {@hasError} returns false.
-	 * 
-	 * @return the error message
-	 */
 	public ErrorMessage getError() {
 		if(!this.hasError()) {
 			error = ErrorMessage.NO_ERROR;
@@ -35,19 +23,21 @@ public class ReturnObjectImpl implements ReturnObject {
 		return this.error;
 	}
 
-	/**
-	 * Returns the object wrapped in this ReturnObject, i.e. the
-	 * result of the operation if it was successful, or null if
-	 * there has been an error.
-	 * 
-	 * Note that the output of this method must be null if {@see
-	 * hasError} returns true, but the opposite is not true: if
-	 * {@see hasError} returns false, this method may or may not
-	 * return null.
-	 * 
-	 * @return the return value from the method or null if there has been an error
-	 */
 	public Object getReturnValue() {
 		return this.object;
 	}
+	
+	public void setObject(Object object) {
+		this.object = object;
+	}
+	
+	public void setError(ErrorMessage error) {
+		this.error = error;
+	}
+	
+	public void printReturnObject() {
+		System.out.println("Object: " + this.getReturnValue());
+		System.out.println("Error: " + this.getError());
+	}
+
 }
