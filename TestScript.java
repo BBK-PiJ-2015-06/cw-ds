3a40037480c90b01ee1bcecc2dc5db1d689ae8c8
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-01 19:44:37 +0000

    New TestScript file created to test whether code works.

---
+++ TestScript.java
@@ -0,0 +1,9 @@
+public class TestScript {
+	public static void main(String[] args) {
+		TestScript launcher = new TestScript();
+		launcher.launch();
+	}
+	public void launch() {
+		ArrayList array1 = new ArrayList();
+	}
+}
