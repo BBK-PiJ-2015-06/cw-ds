89b8434a6cc84bd9de448858930f5f81e3e28929
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-03 10:13:38 +0000

    TestScript used to briefly test LinkedList.

--- TestScript.java
+++ TestScript.java
@@ -4,23 +4,19 @@ public class TestScript {
 		launcher.launch();
 	}
 	public void launch() {
-		ArrayList array1 = new ArrayList();
-		ReturnObjectImpl a = array1.add("Hello");
-		a.printReturnObject();
-		if(array1.isEmpty()) {
-			System.out.println("This structure is empty.");
-		} else {
-			System.out.println("This structure is not empty.");
-		}
-		int size = array1.size();
-		System.out.println("Size: " + size);
-		a = array1.get(0);
-		a.printReturnObject();
-		a = array1.remove(0);
-		a.printReturnObject();
-		
-		size = array1.size();
-		System.out.println("New size: " + size);
-	
+		LinkedList link = new LinkedList();
+		ReturnObject a = link.add("Hello");
+		System.out.println(a.getReturnValue());
+		System.out.println(a.getError());
+		link.add("World");
+		link.add(1, 23);
+		a = link.get(0);
+		System.out.println(a.getReturnValue());
+		System.out.println(a.getError());
+		a = link.remove(2);
+		System.out.println(a.getReturnValue());
+		System.out.println(a.getError());
+		int i = link.size();
+		System.out.println(i);
 	}
 }
