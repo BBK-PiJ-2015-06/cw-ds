d90a40d312b0df7dab1b63ecd87eb2fed28044db
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-03 08:26:45 +0000

    New class Linked List created as implementation of interface List. Methods from list copied into LinkedList.

---
+++ LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList implements List {
+	
+	public boolean isEmpty();
+
+	public int size();
+
+	public ReturnObject get(int index);
+
+	public ReturnObject remove(int index);
+
+	public ReturnObject add(int index, Object item);
+
+	public ReturnObject add(Object item);
+	
+}
