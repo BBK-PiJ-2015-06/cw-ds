d3e326db9b160f606ec73c3a203c8ee3f51119a9
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-03 09:44:17 +0000

    Body of each method completed. Compiles correctly with no syntax errors Yet to test functionality of class.

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,98 @@
 public class LinkedList implements List {
 	
-	public boolean isEmpty();
+	private ObjectNode listStart;
+	
+	public LinkedList() {
+		this.listStart = null;
+	}
+	
+	public boolean isEmpty() {
+		boolean output = false;
+		if(this.listStart == null) {
+			output = true;
+		}
+		return output;
+	}
 
-	public int size();
+	public int size() {
+		int count = 0;
+		ObjectNode temp = this.listStart;
+		if(!this.isEmpty()) {
+			while(temp.getNext() != null) {
+				count++;
+				temp = temp.getNext();
+			}
+		}
+		return count;
+	}
 
-	public ReturnObject get(int index);
+	public ReturnObject get(int index) {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		ObjectNode temp = this.listStart;
+		if(index < 0 || index >= this.size()) {
+			output.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			for(int i = 0; i < index; i++) {
+				temp = temp.getNext();
+			}
+			output.setObject(temp.getObject());
+		}
+		return output;
+	}
 
-	public ReturnObject remove(int index);
+	public ReturnObject remove(int index) {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		ObjectNode temp = this.listStart;
+		if(index < 0 || index >= this.size()) {
+			output.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else {
+			for(int i = 0; i < (index - 1); i++) {
+				temp = temp.getNext();
+			}
+			output.setObject(temp.getNext());
+			temp.setNext(temp.getNext().getNext());
+		}
+		return output;
+	}
 
-	public ReturnObject add(int index, Object item);
+	public ReturnObject add(int index, Object item) {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		if(index < 0 || index >= this.size()) {
+			output.setError(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+		} else if(item == null) {
+			output.setError(ErrorMessage.INVALID_ARGUMENT);
+		} else {
+			ObjectNode newObject = new ObjectNode(item);
+			if(index == 0) {
+				newObject.setNext(this.listStart);
+				this.listStart = newObject;
+			} else {
+				ObjectNode temp = this.listStart;
+				for(int i = 0; i < (index - 1); i++) {
+					temp = temp.getNext();
+				}
+				newObject.setNext(temp.getNext());
+				temp.setNext(newObject);
+			}
+		}
+		return output;
+	}
 
-	public ReturnObject add(Object item);
+	public ReturnObject add(Object item) {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		ObjectNode newObject = new ObjectNode(item);
+		if(item == null) {
+			output.setError(ErrorMessage.INVALID_ARGUMENT);
+		} else if(this.isEmpty()) {
+			listStart = newObject;
+		} else {
+			ObjectNode temp = this.listStart;
+			while(temp.getNext() != null) {
+				temp = temp.getNext();
+			}
+			temp.setNext(newObject);
+		}
+		return output;
+	}
 	
 }
