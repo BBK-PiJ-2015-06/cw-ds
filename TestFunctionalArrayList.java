4658af16204fd1a779d9e48a92ec11b2f6c0f9f6
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-04 13:02:37 +0000

    Method created for testing head() on a non-empty list.

--- TestFunctionalArrayList.java
+++ TestFunctionalArrayList.java
@@ -20,4 +20,17 @@ public class TestFunctionalArrayList {
 		assertEquals(expected, output);
 		assertEquals(expectedError, outputError);
 	}
+	
+	@Test 
+	public void testsHeadMethodOnNonEmptyList() {
+		list.add("Object1");
+		list.add("Object2");
+		ReturnObject a = list.head();
+		Object expected = "Object1";
+		ErrorMessage expectedError = ErrorMessage.NO_ERROR;
+		Object output = a.getReturnValue();
+		ErrorMessage outputError = a.getError();
+		assertEquals(expected, output);
+		assertEquals(expectedError, outputError);
+	}
 }
