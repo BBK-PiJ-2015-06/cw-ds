45dd7bfa4de471fb0c809869235bf1994f7aa379
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-05 09:17:35 +0000

    New class StackImpl created which extends AbstractStack.

---
+++ StackImpl.java
@@ -0,0 +1,58 @@
+public class StackImpl extends AbstractStack {
+	
+	public StackImpl(List list) {
+		super(list);
+	}
+	
+		/**
+	 * Returns true if the stack is empty, false otherwise. 
+	 * 
+	 * @return true if the stack is empty, false otherwise. 
+	 */
+	public boolean isEmpty() {
+		return true;
+	}
+
+	/**
+	 * Returns the number of items currently in the stack.
+	 * 
+	 * @return the number of items currently in the stack
+	 */
+	public int size() {
+		return 0;
+	}
+
+	/**
+	 * Adds an element at the top of the stack. 
+	 * 
+	 * @param item the new item to be added
+	 */
+	public void push(Object item) {
+		
+	}
+
+	/**
+	 * Returns the element at the top of the stack. The stack is
+	 * left unchanged.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
+	public ReturnObject top() {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		return output;
+	}
+
+	/**
+	 * Returns the element at the top of the stack. The element is
+	 * removed frmo the stack.
+	 * 
+	 * @return If stack is not empty, the item on the top is returned. If the
+	 *         stack is empty, an appropriate error.
+	 */
+	public ReturnObject pop() {
+		ReturnObjectImpl output = new ReturnObjectImpl();
+		return output;
+	}
+	
+}
