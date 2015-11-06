e18433a11ee6beaf05b82437b4c2df158c613d17
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-06 14:48:06 +0000

    Change made to reverse() method to ensure copy is made when original list is empty too.

--- ImprovedStackImpl.java
+++ ImprovedStackImpl.java
@@ -1,15 +1,35 @@
+/**
+ * Solution to Part 8 of Assignment 2, PiJ 2015
+ * An ImprovedStackImpl is an implementation of ImprovedStackstack 
+ * which in turn extends Stack with additional methods reverse and
+ * remove. 
+ * 
+ * The class uses a {@see List} as the underlying data structure to 
+ * store the elements on the stack. 
+ * 
+ * Just as in StackImpl, an ImprovedStackImpl using a null list as 
+ * its data structure behaves the same way as a stack using an empty 
+ * list except that elements cannot be pushed to a null list. 
+ * A stack with a null list will always therefore be regarded as 
+ * empty and having a size 0.
+ */
+
 public class ImprovedStackImpl implements ImprovedStack {
 	
+	//Field
 	private List stackList;
 	
+	//Constructor method initiates the field stackList with provided list
 	public ImprovedStackImpl(List list) {
 		this.stackList = list;
 	}
 	
 	/**
-	 * Returns true if the stack is empty, false otherwise. 
+	 * Returns true if the stack is empty or if the stack is using
+	 * an underlying data structure that is null, false otherwise. 
 	 * 
-	 * @return true if the stack is empty, false otherwise. 
+	 * @return true if the stack is empty or list is null, 
+	 *         false otherwise. 
 	 */
 	public boolean isEmpty() {
 		if(this.stackList == null) {
@@ -23,6 +43,7 @@ public class ImprovedStackImpl implements ImprovedStack {
 
 	/**
 	 * Returns the number of items currently in the stack.
+	 * A null list returns a size 0.
 	 * 
 	 * @return the number of items currently in the stack
 	 */
@@ -36,6 +57,10 @@ public class ImprovedStackImpl implements ImprovedStack {
 
 	/**
 	 * Adds an element at the top of the stack. 
+	 *
+	 * This method will do nothing if stack contains a null list,
+	 * but will add an element to index 0 if stack contains an 
+	 * empty list.
 	 * 
 	 * @param item the new item to be added
 	 */
@@ -50,7 +75,8 @@ public class ImprovedStackImpl implements ImprovedStack {
 	 * left unchanged.
 	 * 
 	 * @return If stack is not empty, the item on the top is returned. If the
-	 *         stack is empty, an appropriate error.
+	 *         stack is empty or if the list used within the stack is null an 
+	 *         EMPTY_STRUCTURE ErrorMessage is returned.
 	 */
 	public ReturnObject top() {
 		ReturnObjectImpl output = new ReturnObjectImpl();
@@ -64,10 +90,11 @@ public class ImprovedStackImpl implements ImprovedStack {
 
 	/**
 	 * Returns the element at the top of the stack. The element is
-	 * removed frmo the stack.
+	 * removed from the stack.
 	 * 
 	 * @return If stack is not empty, the item on the top is returned. If the
-	 *         stack is empty, an appropriate error.
+	 *         stack is empty or if the list used within the stack is null an 
+	 *         EMPTY_STRUCTURE ErrorMessage is returned.
 	 */
 	public ReturnObject pop() {
 		ReturnObjectImpl output = new ReturnObjectImpl();
@@ -79,18 +106,26 @@ public class ImprovedStackImpl implements ImprovedStack {
 		}
 		return output;
 	}
-		/**
-	 * Returns a copy of this stack with the items reversed, the top
+	
+	/**
+	 * Returns a copy of this stack but with the items reversed, the top
 	 * elements on the original stack is at the bottom of the new
-	 * stack and viceversa.
+	 * stack and viceversa. 
+	 * 
+	 * If the stack contains a null list, the method simply returns another null
+     * list. If the stack is empty, the method returns an empty stack.	 
 	 * 
 	 * @return a copy of this stack with the items reversed. 
 	 */
 	public ImprovedStack reverse() {
-		if(this.isEmpty()) {
+		if(this.stackList == null) {
 			return this;
+		} else if(this.isEmpty()) {
+			ArrayList emptyList = new ArrayList(); //Creates true copy so changes made to reverse list do not affect original and visa versa.
+			ImprovedStackImpl newStack = new ImprovedStackImpl(emptyList);
+			return newStack;
 		} else {
-			ArrayList reverseList = new ArrayList();
+			ArrayList reverseList = new ArrayList(); //Creates an array list as the data structure to hold the reversed elements.
 			for(int index = (this.size() - 1); index >= 0; index--) {
 				reverseList.add(this.stackList.get(index).getReturnValue());
 			}
@@ -103,8 +138,8 @@ public class ImprovedStackImpl implements ImprovedStack {
 	 * Removes the given object from the stack if it is
 	 * there. Multiple instances of the object are all removed.
 	 *
-	 * Classes implementing this method must use method .equals() to
-	 * check whether the item is in the stack or not.
+	 * This method performs no action if the stack is empty or 
+	 * contains a null list.
 	 * 
 	 * @param object the object to remove
 	 */
