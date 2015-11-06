5b0fadfde1022d5bce810d824dfb16d11f1f30d6
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-06 11:19:20 +0000

    Comments added to ReturnObjectImpl

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,3 +1,11 @@
+/**
+ * Solution to Part 1 of Assignment 2, PiJ 2015
+ * An implementation of the interface ReturnObject,
+ * containing either an object (the result of an operation
+ * on a data structure) or an error value.
+ *
+ */
+
 public class ReturnObjectImpl implements ReturnObject {
 	
 	private Object object;
@@ -8,6 +16,10 @@ public class ReturnObjectImpl implements ReturnObject {
 		this.error = null;
 	}
 	
+	/**
+	 * Returns whether there has been an error
+	 * @return true if there has been an error otherwise returns false
+	 */
 	public boolean hasError() {
 		boolean output = false;
 		if(this.error != null) {
@@ -16,6 +28,14 @@ public class ReturnObjectImpl implements ReturnObject {
 		return output;
 	}
 
+	/**
+	 * Returns the error message. 
+	 * 
+	 * Return NO_ERROR if {@hasError} returns false.
+	 * otherwise returns the current error message of the object
+	 * 
+	 * @return the error message
+	 */
 	public ErrorMessage getError() {
 		if(!this.hasError()) {
 			error = ErrorMessage.NO_ERROR;
@@ -23,14 +43,39 @@ public class ReturnObjectImpl implements ReturnObject {
 		return this.error;
 	}
 
+	/**
+	 * Returns the object.
+	 * 
+	 * Returns null if there has been an error or
+	 * if there is no object to return from the method
+	 * 
+	 * @return the return value from the method
+	 */
 	public Object getReturnValue() {
 		return this.object;
 	}
 	
+	/**
+	 * Sets the value of object.
+	 *
+	 * Enables methods within other classes to set the value of their
+	 * ReturnObject objects.
+	 *
+	 * @param object the object that this object is to be set to
+	 */
 	public void setObject(Object object) {
 		this.object = object;
 	}
 	
+	/**
+	 * Sets the value of this error. 
+	 *
+	 * Enables methods within other classes to set the value of their
+	 * ReturnObject errors. The set of values used can only be those
+	 * found in the ErrorMessage enum class.
+	 *
+	 * @param error the ErrorMessage that this error is to be set to
+	 */
 	public void setError(ErrorMessage error) {
 		this.error = error;
 	}
